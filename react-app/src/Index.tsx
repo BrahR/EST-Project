16e36a693dae8abc4b696c449e5fcb726a54a1b9
BrahR <44587812+BrahR@users.noreply.github.com>
2023-11-20 16:05:42 -0800

    Moved main to Index

---
+++ react-app/src/Index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RouteHandeler from "@/routeHandeler";
+import "@/assets/css/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouteHandeler />
+  </React.StrictMode>
+);
--- react-app/src/routeHandeler.tsx
+++ react-app/src/routeHandeler.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Login from "./login"
-import Dashboard from "./admin/dashboard"
-import Departement from "./admin/departement/departements"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "@/admin/dashboard";
+import Departement from "@/admin/departement/departements";
+import Login from "@/views/login";
 
 export default function RouteHandeler() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                {/* Departement */}
-                <Route path="/deparetement" element={<Departement />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Departement */}
+        <Route path="/deparetement" element={<Departement />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
